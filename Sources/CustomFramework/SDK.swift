9c4d9b630683303e21b1843775208f2280b31cf9
Daniel Kerbel <dannykerbel@gmail.com>
2024-02-25 23:40:00 -0500

    Initial Commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Daniel Kerbel on 2/25/24.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
